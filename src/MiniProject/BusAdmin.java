44b6a5a6a7f74cc10776b0a2ff4e60b993787f4c
Mohankumar <mohanmk443@mail.com>
2024-12-19 14:41:30 +0530

    first commit

--- src/MiniProject/BusAdmin.java
+++
@@ -1,9 +0,0 @@
-package MiniProject;
-
-public class BusAdmin extends BusOwner {
-
-	public void InvestAmount(double amount) {
-		super.InvestAmount(amount);
-	}
-	
-}
--- src/MiniProject/BusOwner.java
+++
@@ -1,9 +0,0 @@
-package MiniProject;
-
-public class BusOwner {
-	
-	static double amount = 100000.0;
-	public void InvestAmount(double amount) {
-		BusOwner.amount=amount;
-	}
-}
